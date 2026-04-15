91cd9afb072cc55ba72e384e7ae77c226c9a0bc8
Malena Rios <mariamagdalenarios22@gmail.com>
2026-04-15 19:23:54 +0200

    Fix NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage.tsx'
 import { NotFoundPage } from './pages/NotFoundPage.tsx'
+import { NavBar } from './components/NavBar.tsx'
+import { ReportsPage } from './pages/Reports.tsx'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<Navigate to="/" replace />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/reports" element={<ReportsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   )
 }
 
