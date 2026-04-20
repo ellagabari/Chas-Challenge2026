3d07fdaa01478fa289828f07c697ea6a9e8f48d7
Hugo <hugo.oblak@outlook.com>
2026-04-20 14:09:19 +0200

    merged the latest version of dev into feature/signin-signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,11 +17,8 @@ function App() {
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/reports" element={<ReportsPage />} />
         <Route path="/profile" element={<UserProfile />} />
-<<<<<<< HEAD
         <Route path="/login" element={<LoginPage />} />
-=======
         <Route path="/leaderboard" element={<LeaderboardPage />} />
->>>>>>> df3dad959fa194ddc2d23ccd23069fadab7e4c75
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
