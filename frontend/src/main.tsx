4887013ed2114eebd34fd8296922166681686017
ErnestIssa <ernestisa32@gmail.com>
2026-04-21 10:17:55 +0200

    bug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
+// 1. Add these imports
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
-
-// 1. Add these imports
-=======
->>>>>>> cd51f4617f9464fa8aaa7fa96fe548e698b65a8d
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
